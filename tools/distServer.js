9560e6d8a6a051bf43aab741e00f0accd1adb3d9
minha-neofect <minha@neofect.com>
2017-02-02 22:58:27 +0900

    일부 수정

--- tools/distServer.js
+++ tools/distServer.js
@@ -5,7 +5,7 @@ import compression from 'compression';
 
 /*eslint-disable no-console */
 
-const port = 3000;
+const port = 9999;
 const app = express();
 
 app.use(compression());
@@ -19,6 +19,6 @@ app.listen(port, function(err) {
   if (err) {
     console.log(err);
   } else {
-    open(`http://localhost:${port}`);
+    open(`http://139.162.106.240:${port}`);
   }
 });
